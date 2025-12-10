195f62b26c7e9461692ee58962b1357cfd5bd90e
B0mf11m <joaovitorbomfim0@gmail.com>
2025-12-09 21:17:28 -0300

    inicio do projeto

---
+++ conceitos_Basicos.py
@@ -0,0 +1,20 @@
+#===============================VARIÁVEIS=======================================
+# Variáveis: um espaço no computador para armazenar um dado
+# Operadores: Soma(+); Subtração(-); Multiplicação(*); Divisão(/); Divisão Inteira(//); Modulo/resto(%); Exponencial(**)
+
+
+
+#vamos para alguns exemplos: 
+
+valor = 10 # Aqui temos uma variável padrão com um valor numérico, este é um numero Inteiro (int)
+valor2=10.5 # Aqui temos uma variável padrão com um valor numérico, este é um numero Decimal (float)
+nome = 'User' # Aqui temos uma variável padrão com um Texto (str)
+tipo = True # Aqui temos uma variável do tipo lógica  (ou seja com um valor verdadeiro ou falso ) (bool)
+
+print("")# Comando {print}, utilizado para exibir mensagens no terminal de comando, tudo que estiver entre ("") será exibido
+print('tipos de variáveis') #exibe a mensagem "tipos de variáveis"
+print(type(valor)) #exibe no terminal o Tipo da variável {valor}
+print(type(valor2)) #exibe no terminal o Tipo da variável {valor2}
+print(type(nome)) #exibe no terminal o Tipo da variável {nome}
+print(type(tipo)) #exibe no terminal o Tipo da variável {tipo}
+#===============================VARIÁVEIS=======================================
